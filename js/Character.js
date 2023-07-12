dc0a95099a885aa45a72d05647f6767f7587f87b
AwabSaif <awabtaz9090@gmail.com>
2023-07-12 21:53:49 +0300

    style css and more adds

--- js/Character.js
+++ js/Character.js
@@ -2,52 +2,55 @@ import { getDiceRollArray, getDicePlaceholderHtml , getPercentage} from "./utils
 
 
 
-function Character(data) {
-    Object.assign(this, data)
-    
-    this.diceHtml = getDicePlaceholderHtml(this.diceCount)
-    
-    this.getHealthBarHtml = ()=>{
-        const percent = getPercentage(this.health ,this.maxHealth)
-        return `
-        <div class="health-bar-outer">
+class Character {
+    constructor(data){
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+    }
+        
+        
+       getHealthBarHtml (){
+            const percent = getPercentage(this.health ,this.maxHealth)
+            return `
+            <div class="health-bar-outer">
             <div class="health-bar-inner ${percent <26 ?"danger" :""} " 
             style="width:${percent}%;">
            </div>
-         </div>`
-
-        
-    }
-    this.maxHealth = this.health
-    this.setDiceHtml =  () => {
-        this.currentDiceScore = getDiceRollArray(this.diceCount)
-        this.diceHtml = this.currentDiceScore.map( num => `<div class="dice">${num}</div>` ).join('')
-        
-    }
-    this.takeDamage = function (attackScoreArray) {
-        const totalAttackScore = attackScoreArray.reduce( (total, num) => total + num)
-        this.health -= totalAttackScore
-        if (this.health <= 0) {
-            this.dead = true
-            this.health = 0
+           </div>`
+           
+           
         }
-    }
+        setDiceHtml (){
+            this.currentDiceScore = getDiceRollArray(this.diceCount)
+            this.diceHtml = this.currentDiceScore.map( num => `<div class="dice">${num}</div>` ).join('')
+            
+        }
+        takeDamage (attackScoreArray) {
+            const totalAttackScore = attackScoreArray.reduce( (total, num) => total + num)
+            this.health -= totalAttackScore
+            if (this.health <= 0) {
+                this.dead = true
+                this.health = 0
+            }
+        }
+        
+        getCharacterHtml() {
+            const { elementId, name, avatar, health, diceCount, diceHtml } = this;
+            const  healthBar =  this.getHealthBarHtml()
+            
+            return `
+            <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img src="${avatar}"  class="avatar">
+            <p class="health">health: <b> ${health} </b></p>
+            ${healthBar}
+            <div class="dice-container">
+            ${diceHtml}
+            </div>`;
+        }
+        
     
-    this.getCharacterHtml =  () => {
-        const { elementId, name, avatar, health, diceCount, diceHtml } = this;
-        const  healthBar =  this.getHealthBarHtml()
-     
-        return `
-          <div class="character-card">
-          <h4 class="name"> ${name} </h4>
-          <img src="${avatar}"  class="avatar">
-          <p class="health">health: <b> ${health} </b></p>
-          ${healthBar}
-          <div class="dice-container">
-          ${diceHtml}
-      </div>`;
-    }
-
 }
 
 
